1439e7ef0b832c9c297a5a880c0535289b6840fe
xvanov <xvanov@gmail.com>
2020-12-28 13:54:50 -0800

    integrated issues with predictions

--- issue_results_api.py
+++ issue_results_api.py
@@ -5,7 +5,7 @@ import re
 
 # local dependencies
 import dirtools
-
+from nstools import NationStatesAPI
 # main class
 class IssuePredictionAPI():
     def __init__(self, dirs):
@@ -45,11 +45,32 @@ class IssuePredictionAPI():
                     meanparam = re.findall('\d[^\d]*\(',line)[0][2:-2]
                     optionDict[meanparam] = [minimum, maximum, mean]
             predictionsList.append(optionDict)
-            
+
         return predictionsList
 # run
 if __name__ == '__main__':
-    natioName = 'north_kalandia'
+    nationName = 'north_kalandia'
     baseDir = os.getcwd()
     dirs = dirtools.Dirs(baseDir, nationName)
+
+    # get issues
+    nationName = 'north_kalandia'
+    headers = {'User-Agent':'Nation Info Getter', "X-Password": "koraxhun123"}
+
+    urlStats = "https://www.nationstates.net/cgi-bin/api.cgi?nation=north_kalandia&q=census;scale=all;mode=score"
+
+    baseDir = os.getcwd()
+    dirs = dirtools.Dirs(baseDir, nationName)
+    nationStatesClass = NationStatesAPI(dirs, headers)
+    urlIssues = nationStatesClass.generate_issues_url()
+    issuesList = nationStatesClass.get_nation_issues(dirs.issuesDir, urlIssues)
+    print(issuesList)
+
+
     ipa = IssuePredictionAPI(dirs)
+    predictions = []
+    for issue in issuesList:
+        ipa.get_predictions(issue)
+        preds = ipa.process_predictions()
+        predictions.append(preds)
+    print(predictions)
